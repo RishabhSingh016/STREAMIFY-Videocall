4a7c14c71639f15325072d8e5b4495f38e059b50
RishabhSingh016 <rishabhsingh1674@gmail.com>
2025-08-23 00:28:19 +0530

    modified

--- backend/src/server.js
+++ backend/src/server.js
@@ -31,10 +31,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
